ebe4cf2567eb021d4e3becfae86db49795f60832
Dimka863 <dlysachenko98@gmail.com>
2024-03-28 13:15:06 +0100

    wqeq

--- js/training.js
+++ js/training.js
@@ -1,2 +1,16 @@
+const clickBtn = document.querySelector(".clk");
+const resetBtn = document.querySelector(".rst");
+const square = document.querySelector(".square");
+let movement = 0;
+function movementFunc() {
+  movement += 100;
+  square.style.transform = `translate(${movement}px, ${movement}px)`;
+}
+function resetFunc() {
+  square.style.transform = `translate(0px, 0px)`;
+  movement = 0;
+}
+clickBtn.addEventListener("click", movementFunc);
+resetBtn.addEventListener("click", resetFunc);
 
-
+console.dir(window);
